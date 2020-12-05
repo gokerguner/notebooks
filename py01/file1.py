f6face9def390efd996e8bf5c085b4b19154c4a3
gokerguner <gokerguner67@gmail.com>
2020-12-06 01:09:16 +0300

    İlk Python okumasının notebook'u.

---
+++ py01/file1.py
@@ -0,0 +1,8 @@
+# file1.py
+def addition(n1, n2):
+    print(__name__)
+    return n1 + n2
+
+
+if __name__ == "__main__":
+    print("Hello, I'm file1.py")
---
+++ py01/file2.py
@@ -0,0 +1,5 @@
+# file2.py
+import file1
+
+print(file1.addition(5, 3))
+print(__name__)
